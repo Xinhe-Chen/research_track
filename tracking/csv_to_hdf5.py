6a494db1ebdd2bae13ab719562d02799f2c2e1fc
Xinhe Chen <xchen24@nd.edu>
2022-09-21 10:23:37 -0400

    unable to convert csv to hdf5

--- tracking/csv_to_hdf5.py
+++ tracking/csv_to_hdf5.py
@@ -13,6 +13,6 @@ filename = 'Dispatch_shuffled_data_0.h5'
 df_dispatch.to_hdf(filename, 'df', mode='w', format='table')
 print('convert completed')
 
-# conclusion: unable to work
+# conclusion: unable to work because of limitation of hdf5 files header
 # hdf5 files has limitation of header of 64kb (about 2000 colunms)
 # https://stackoverflow.com/questions/27203161/convert-large-csv-to-hdf5
